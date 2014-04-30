ebfff79804667c95f84d10d8d8d11b178a6741d4
iwasakims <iwasakims@example.com>
2014-04-30 14:43:32 -0700

    successfully started 2 nodes.

--- local-resourcemanagers.sh
+++ local-resourcemanagers.sh
@@ -1,4 +1,29 @@
- bin=`dirname "${BASH_SOURCE-$0}"`
+#!/bin/sh
+#/**
+# * Copyright 2007 The Apache Software Foundation
+# *
+# * Licensed to the Apache Software Foundation (ASF) under one
+# * or more contributor license agreements.  See the NOTICE file
+# * distributed with this work for additional information
+# * regarding copyright ownership.  The ASF licenses this file
+# * to you under the Apache License, Version 2.0 (the
+# * "License"); you may not use this file except in compliance
+# * with the License.  You may obtain a copy of the License at
+# *
+# *     http://www.apache.org/licenses/LICENSE-2.0
+# *
+# * Unless required by applicable law or agreed to in writing, software
+# * distributed under the License is distributed on an "AS IS" BASIS,
+# * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# * See the License for the specific language governing permissions and
+# * limitations under the License.
+# */
+# This is used for starting multiple masters on the same machine.
+# run it from hbase-dir/ just like 'bin/hbase'
+# Supports up to 10 masters (limitation = overlapping ports)
+set -x
+
+bin=`dirname "${BASH_SOURCE-$0}"`
 bin=`cd "$bin" >/dev/null && pwd`
 
 if [ $# -lt 2 ]; then
@@ -18,20 +43,20 @@ run_master () {
   shift
   RMID="rm${DN}"
   OFFSET=`expr $DN \* 100`
-  HOST=`hostname`
+  ADDRESS="0.0.0.0"
   export YARN_IDENT_STRING="$USER-$DN"
   YARN_RESOURCEMANAGER_ARGS="$@ \
     -Dyarn.resourcemanager.ha.enabled=true \
     -Dyarn.resourcemanager.ha.automatic-failover.enabled=false \
     -Dyarn.resourcemanager.cluster-id=cl1 \
     -Dyarn.resourcemanager.ha.id=${RMID} \
-    -Dyarn.resourcemanager.webapp.address.${RMID}=$HOST:`expr 8088 + $OFFSET` \
-    -Dyarn.resourcemanager.scheduler.address.${RMID}=$HOST:`expr 8030 + $OFFSET` \
-    -Dyarn.resourcemanager.resource-tracker.address.${RMID}=$HOST:`expr 8031 + $OFFSET` \
-    -Dyarn.resourcemanager.address.${RMID}=$HOST:`expr 8032 + $OFFSET` \
-    -Dyarn.resourcemanager.admin.address.${RMID}=$HOST:`expr 8033 + $OFFSET` "
+    -Dyarn.resourcemanager.webapp.address.${RMID}=$ADDRESS:`expr 8088 + $OFFSET` \
+    -Dyarn.resourcemanager.scheduler.address.${RMID}=$ADDRESS:`expr 8030 + $OFFSET` \
+    -Dyarn.resourcemanager.resource-tracker.address.${RMID}=$ADDRESS:`expr 8031 + $OFFSET` \
+    -Dyarn.resourcemanager.address.${RMID}=$ADDRESS:`expr 8032 + $OFFSET` \
+    -Dyarn.resourcemanager.admin.address.${RMID}=$ADDRESS:`expr 8033 + $OFFSET` "
   "$bin"/yarn-daemon.sh --config "${YARN_CONF_DIR}" \
-    "${COMMAND}" resourcemanager "${YARN_RESOURCEMANAGER_ARGS}"
+    "${COMMAND}" resourcemanager ${YARN_RESOURCEMANAGER_ARGS}
 }
 
 cmd=$1
