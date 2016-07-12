af7f138281e90aa50e527a5fe6976c04583e90a1
Masatake Iwasaki <iwasakims@example.org>
2016-07-12 11:28:45 +0000

    fixed hbase library setting.

--- ansible/hadoop2/roles/hbase/templates/hbase-env.sh
+++ ansible/hadoop2/roles/hbase/templates/hbase-env.sh
@@ -135,3 +135,5 @@ export HBASE_REGIONSERVER_OPTS="$HBASE_REGIONSERVER_OPTS -XX:PermSize=128m -XX:M
 # HBASE_ROOT_LOGGER=INFO,DRFA
 # The reason for changing default to RFA is to avoid the boundary case of filling out disk space as 
 # DRFA doesn't put any cap on the log size. Please refer to HBase-5655 for more context.
+
+export HADOOP_HOME={{ hadoop_home }}
